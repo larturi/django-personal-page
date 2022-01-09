e571929fca8cbc678ff4767b2de8fa0b9055cfb7
Leandro Arturi <lea.arturi@gmail.com>
2022-01-08 23:37:22 -0300

    Panel Administrativo

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'core',
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Fields
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
